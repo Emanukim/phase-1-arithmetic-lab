4201e09e37b4e6962f5ca419e128692e74a880eb
Emmanuel <emmanue.kimweli@student.moringaschool.com>
2022-08-19 22:35:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 10;
+let num2 = 6.2;
+let num3 = 20;
+let num4 = 16;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*10);
+
+let mod = num3 % num4;
+
+let max = Math.max(num1, num2, num3, num4);
